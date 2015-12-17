bf46b16a36762fbd5f5708c71b432d3dc4bab00d
tomaszjanicki <tomaszjanicki@ciber.com>
2015-12-17 15:03:52 +0100

    xx

--- WebApplication3/Static/Scripts/ts/App_Start/requiresetup.ts
+++ WebApplication3/Static/Scripts/ts/App_Start/requiresetup.ts
@@ -6,8 +6,8 @@ requirejs.config(
             "bootstrap": { "deps": ["jquery"] }
         },
         paths: {
-            "jquery": "Static/Scripts/js/jquery-bundle.min",
-            "bootstrap": "Static/Scripts/js/bootstrap-bundle.min",
-            "knockout": "Static/Scripts/js/knockout-bundle.min"
+            "jquery": "/Static/Scripts/js/jquery-bundle.min",
+            "bootstrap": "/Static/Scripts/js/bootstrap-bundle.min",
+            "knockout": "/Static/Scripts/js/knockout-bundle.min"
         }
     });
