18fb4a450aa6e44a187751a5a5d6352a95ba3980
tomaszjanicki <tomaszjanicki@ciber.com>
2015-12-30 12:09:26 +0100

    x

---
+++ projects/WebApplication3/Static/Scripts/ts/Views/Home/Index/Main.ts
@@ -0,0 +1,15 @@
+﻿/// <reference path="../../../../typings/tsd.d.ts" />
+
+import TestVm = require("./TestVm");
+import jQuery = require("jquery");
+import ko = require("knockout");
+
+require(["bootstrap"], () => {
+    jQuery(document).ready(() => {
+        jQuery("#jqueryButton").click(() => {
+            window.alert("Alert from jquery");
+        });
+    });
+
+    ko.applyBindings(new TestVm());
+})
---
+++ projects/WebApplication3/Static/Scripts/ts/Views/Home/Index/TestVm.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="../../../../typings/tsd.d.ts" />
+
+import ko = require("knockout");
+
+class TestVm {
+    projectType = ko.observable("New project type");
+
+    clickMe = (): void => {
+        this.projectType("Very new project type");
+        window.alert("Alert from knockout");
+    }
+}
+
+export = TestVm;
