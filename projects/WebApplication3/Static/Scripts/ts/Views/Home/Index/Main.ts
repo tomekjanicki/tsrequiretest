852112e9fb5b37cd0652f9583358c93208d14587
tomaszjanicki <tomaszjanicki@ciber.com>
2016-01-04 13:52:05 +0100

    xxx

--- projects/WebApplication3/Static/Scripts/ts/Views/Home/Index/Main.ts
+++ projects/WebApplication3/Static/Scripts/ts/Views/Home/Index/Main.ts
@@ -1,4 +1,4 @@
-﻿/// <reference path="../../../../typings/tsd.d.ts" />
+﻿/// <reference path="../../../../../../../../typings/tsd.d.ts" />
 
 import TestVm = require("./TestVm");
 import $ = require("jquery");
