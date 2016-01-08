eb6f8768ee00d72b2348aad05fcca3861f204a59
tomaszjanicki <tomaszjanicki@ciber.com>
2016-01-08 11:46:22 +0100

    add signal r core

---
+++ projects/WebApplication3/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+
+namespace WebApplication3
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+
+    }
+}
