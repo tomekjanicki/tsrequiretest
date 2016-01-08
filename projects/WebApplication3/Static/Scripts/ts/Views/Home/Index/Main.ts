71e5701d4c2ffadb98b586221ebdaedf2de7aeb5
tomaszjanicki <tomaszjanicki@ciber.com>
2016-01-09 00:07:16 +0100

    yyyy

--- projects/WebApplication3/Static/Scripts/ts/Views/Home/Index/Main.ts
+++ projects/WebApplication3/Static/Scripts/ts/Views/Home/Index/Main.ts
@@ -32,7 +32,7 @@ require(["signalrhubs"], () => {
             $("#discussion").append(`<li><strong>${encodedName}</strong>:&nbsp;&nbsp;${encodedMsg}</li>`);
         };
         // Get the user name and store it to prepend to messages.
-        $("#displayname").val(prompt('Enter your name:', ''));
+        $("#displayname").val(prompt("Enter your name:", ""));
         // Set initial focus to message input box.
         $("#message").focus();
         // Start the connection.
